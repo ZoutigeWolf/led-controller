9b16d3657fa779d58a571056e7e78bd98d0053a5
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-12 00:57:03 +0200

    damn

--- increase.py
+++ increase.py
@@ -12,22 +12,29 @@ LENGTH = 84
 
 pixels = neopixel.NeoPixel(board.D18, LENGTH, auto_write=False)
 
-i = 0
 
-while True:
-    (r, g, b) = colorsys.hsv_to_rgb(remap(i, 0, LENGTH - 1, 0, 1), 1, 1)
-    pixels[i] = (
-        int(remap(r, 0, 1, 0, 255)),
-        int(remap(g, 0, 1, 0, 255)),
-        int(remap(b, 0, 1, 0, 255))
-    )
+def increase(decrease):
+    for i in range(LENGTH - 1, -1, -1) if decrease else range(LENGTH):
+        (r, g, b) = colorsys.hsv_to_rgb(remap(i, 0, LENGTH - 1, 0, 1), 1, 1)
+        pixels[i] = (
+            int(remap(r, 0, 1, 0, 255)),
+            int(remap(g, 0, 1, 0, 255)),
+            int(remap(b, 0, 1, 0, 255))
+        ) if not decrease else (0, 0, 0)
 
-    if i == 83:
-        pixels.fill((0, 0, 0))
-        i = -1
+        pixels.show()
 
-    pixels.show()
+        if i == 83 and not decrease:
+            increase(True)
+            return
 
-    i += 1
+        elif i == 0 and decrease:
+            increase(False)
+            return
 
-    time.sleep(0.1)
+        i += 1
+
+        time.sleep(0.1)
+
+
+increase(False)
