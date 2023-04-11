112e7c36fe12f4938f830a292fc818274224d7c3
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 20:36:31 +0200

    damn

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ offset = 0
 
 while True:
     for i in range(LENGTH):
-        pixels[i] = COLORS[(int(remap(i, 0, LENGTH, 0, 255)) + offset) % 255]
+        pixels[i] = COLORS[(i * 10 + offset) % 255]
 
-    offset += 10
+    offset += 25
