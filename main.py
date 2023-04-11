78f4d9ee6c9a75719cfdfe03eb99d354019da23c
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 20:39:49 +0200

    damn

--- main.py
+++ main.py
@@ -6,30 +6,7 @@ def remap(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-def generate_rainbow_colors():
-    rainbow_colors = []
-    for i in range(255):
-        if i < 85:
-            r = 255
-            g = int(255 * (i / 85))
-            b = 0
-        elif i < 170:
-            i -= 85
-            r = int(255 * ((85 - i) / 85))
-            g = 255
-            b = int(255 * (i / 85))
-        else:
-            i -= 170
-            r = 0
-            g = int(255 * ((85 - i) / 85))
-            b = 255
-        rainbow_colors.append((r, g, b))
-    return rainbow_colors
-
-
-
-
-COLORS = generate_rainbow_colors()
+COLORS = [(255, 0, 0), (255, 85, 0), (255, 170, 0), (255, 255, 0), (170, 255, 0), (85, 255, 0), (0, 255, 0), (0, 255, 85), (0, 255, 170), (0, 255, 255), (0, 170, 255), (0, 85, 255), (0, 0, 255), (85, 0, 255), (170, 0, 255), (255, 0, 255)]
 
 LENGTH = 90
 
