657e68907885fbdb1ae6eb6121db55b2f75130e4
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 20:20:23 +0200

    damn

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ import neopixel
 
 def generate_rainbow_colors():
     rainbow_colors = []
-
     for i in range(255):
         r = int(255 * ((i / 255) ** 2))
         g = int(255 * ((1 - ((i / 255) - 0.5) ** 2) ** 0.5))
-        b = int(255 * (((i / 255) - 1) ** 2))
+        b = int(255 * (((255 - i) / 255) ** 2))
         rainbow_colors.append((r, g, b))
-
     return rainbow_colors
 
 
@@ -27,4 +25,4 @@ while True:
     for i in range(LENGTH):
         pixels[i] = COLORS[(offset + i) % 255]
 
-    offset += 1
+    offset += 10
