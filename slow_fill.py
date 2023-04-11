0a63aaa513a8318f9f9a37a3c9118205351e0058
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 21:52:47 +0200

    damn

--- slow_fill.py
+++ slow_fill.py
@@ -14,14 +14,16 @@ pixels = neopixel.NeoPixel(board.D18, LENGTH, auto_write=False)
 i = 0
 
 while True:
-    (r, g, b) = colorsys.hsv_to_rgb(remap(i % LENGTH, 0, LENGTH - 1, 0, 1), 1, 1)
-    pixels[i % LENGTH] = (
+    index = i % LENGTH
+
+    (r, g, b) = colorsys.hsv_to_rgb(remap(index, 0, LENGTH - 1, 0, 1), 1, 1)
+    pixels[index] = (
         int(remap(r, 0, 1, 0, 255)),
         int(remap(g, 0, 1, 0, 255)),
         int(remap(b, 0, 1, 0, 255))
     )
 
-    if i > 0:
+    if index > 0:
         pixels[i - 1] = (0, 0, 0)
 
     pixels.show()
