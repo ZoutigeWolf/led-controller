3f0e7f48e1f218a3404bd98fe55ae579eebaec8a
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 21:40:48 +0200

    damn

--- rainbow_wave.py
+++ rainbow_wave.py
@@ -4,7 +4,7 @@ import neopixel
 
 
 def remap(x, in_min, in_max, out_min, out_max):
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 LENGTH = 90
