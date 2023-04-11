15c8055d2927bbd0ac4744f810dc23354516a4ac
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 19:37:48 +0200

    damn

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import neopixel
 
 LENGTH = 90
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = 0x100000
+GREEN = 0x001000
+BLUE = 0x000010
+
+WHITE = RED | GREEN | BLUE
 
 pixels = neopixel.NeoPixel(board.D18, LENGTH)
 
 i = 0
 
 while True:
-    pixels.fill((0, 0, 0))
+    pixels.fill(WHITE)
 
     v = i % LENGTH
 
-    pixels[v] = (255, 0, 255)
+    pixels[v] = BLUE | RED
 
     if (v - 1) >= 0:
-        pixels[v - 1] = (255, 0, 255)
+        pixels[v - 1] = BLUE | RED
 
     if (v + 1) < LENGTH:
-        pixels[v + 1] = (255, 0, 255)
+        pixels[v + 1] = BLUE | RED
 
     i += 1
     time.sleep(0.005)
