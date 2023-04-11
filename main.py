269fe93608c3c7c1119703935bdf73bd64925899
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-11 20:28:11 +0200

    damn

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ def generate_rainbow_colors():
     rainbow_colors = []
     for i in range(255):
         if i < 85:
-            r = int(255 * (i / 85))
-            g = int(255 * (i / 85))
-            b = int(255 * ((85 - i) / 85))
+            r = 255
+            g = int(255 * ((85 - i) / 85))
+            b = 0
         elif i < 170:
             i -= 85
-            r = int(255 * (i / 85))
-            g = int(255 * ((85 - i) / 85))
+            r = int(255 * ((85 - i) / 85))
+            g = int(255 * (i / 85))
             b = 0
         else:
             i -= 170
-            r = int(255 * ((85 - i) / 85))
-            g = 0
+            r = 0
+            g = int(255 * ((85 - i) / 85))
             b = int(255 * (i / 85))
         rainbow_colors.append((r, g, b))
     return rainbow_colors
