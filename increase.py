1cb06b1d2c8918dfc86ac04cf6093a773a5a41ea
ZoutigeWolf <guuskamphuis@gmail.com>
2023-04-12 00:49:47 +0200

    damn

--- increase.py
+++ increase.py
@@ -13,29 +13,17 @@ LENGTH = 84
 pixels = neopixel.NeoPixel(board.D18, LENGTH, auto_write=False)
 
 i = 0
-end = LENGTH
 
 while True:
-    if end == 0:
-        end = LENGTH
-
-    index = i % end
-
-    (r, g, b) = colorsys.hsv_to_rgb(remap(index, 0, LENGTH - 1, 0, 1), 1, 1)
-    pixels[index] = (
+    (r, g, b) = colorsys.hsv_to_rgb(remap(i, 0, LENGTH - 1, 0, 1), 1, 1)
+    pixels[i] = (
         int(remap(r, 0, 1, 0, 255)),
         int(remap(g, 0, 1, 0, 255)),
         int(remap(b, 0, 1, 0, 255))
     )
 
-    if index == end - 1:
-        end -= 1
+    if i == 83:
+        pixels.fill((0, 0, 0))
+        i = -1
 
-    if index == 0:
-        pixels[LENGTH - 1] = (0, 0, 0)
-    else:
-        pixels[index - 1] = (0, 0, 0)
-
-    pixels.show()
     i += 1
-    #time.sleep(0.01)
--- rainbow_wave.py
+++ rainbow_wave.py
@@ -7,7 +7,7 @@ def remap(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-LENGTH = 90
+LENGTH = 84
 
 pixels = neopixel.NeoPixel(board.D18, LENGTH, auto_write=False)
 
